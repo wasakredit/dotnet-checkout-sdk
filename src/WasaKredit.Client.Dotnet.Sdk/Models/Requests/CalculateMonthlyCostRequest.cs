da1307b983a7ff4b880fb6ab5b34af056a10a569
elias.ung <elias.ung@starrepublic.com>
2018-03-13 12:36:35 +0100

    Fix namespace

--- src/WasaKredit.Client.Dotnet.Sdk/Models/Requests/CalculateMonthlyCostRequest.cs
+++ src/WasaKredit.Client.Dotnet.Sdk/Models/Requests/CalculateMonthlyCostRequest.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
+using WasaKredit.Client.Dotnet.Sdk.Models;
 
-namespace WasaKredit.Client.Dotnet.Sdk.Models.Requests
+namespace WasaKredit.Client.Dotnet.Sdk.Requests
 {
     public class CalculateMonthlyCostRequest
     {
