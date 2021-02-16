fc52d377aba765858d018f564d75f7a5d497292f
kahrewk <61494065+kahrewk@users.noreply.github.com>
2021-02-16 16:38:22 +0100

    Fix protocol

--- src/WasaKredit.Client.Dotnet.Sdk/Settings.cs
+++ src/WasaKredit.Client.Dotnet.Sdk/Settings.cs
@@ -5,7 +5,7 @@
         public const string AUTHENTICATION_URL = "https://b2b.services.wasakredit.se/auth/connect/token";
         public const string CHECKOUT_GATEWAY_API_URL = "https://b2b.services.wasakredit.se";
 
-        public const string TEST_AUTHENTICATION_URL = "http://auth.inttest-b2b.wasakredit.se/connect/token";
-        public const string TEST_CHECKOUT_GATEWAY_API_URL = "http://api.inttest-b2b.wasakredit.se";
+        public const string TEST_AUTHENTICATION_URL = "https://auth.inttest-b2b.wasakredit.se/connect/token";
+        public const string TEST_CHECKOUT_GATEWAY_API_URL = "https://api.inttest-b2b.wasakredit.se";
     }
 }
