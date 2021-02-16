eaa101ca35d0c394b8ed964fcfb314e4bc37f10b
kahrewk <61494065+kahrewk@users.noreply.github.com>
2021-02-16 16:31:12 +0100

    Update with new test environment url

--- src/WasaKredit.Client.Dotnet.Sdk/Settings.cs
+++ src/WasaKredit.Client.Dotnet.Sdk/Settings.cs
@@ -5,7 +5,7 @@
         public const string AUTHENTICATION_URL = "https://b2b.services.wasakredit.se/auth/connect/token";
         public const string CHECKOUT_GATEWAY_API_URL = "https://b2b.services.wasakredit.se";
 
-        public const string TEST_AUTHENTICATION_URL = "http://auth.internalsvc-test.wasakredit.se:8080/connect/token";
-        public const string TEST_CHECKOUT_GATEWAY_API_URL = "http://internalsvc-test.wasakredit.se:8080/api";
+        public const string TEST_AUTHENTICATION_URL = "http://auth.inttest-b2b.wasakredit.se/connect/token";
+        public const string TEST_CHECKOUT_GATEWAY_API_URL = "http://api.inttest-b2b.wasakredit.se";
     }
 }
